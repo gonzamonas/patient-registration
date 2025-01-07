b86082c78c36e106f7f6e0818f2f1617429a917e
Gonzalo Monasterio <gonzalo.monasterio@fcen.uba.ar>
2025-01-07 03:25:21 -0300

    Add controller with validations and exception handling

--- app/main.py
+++ app/main.py
@@ -1,8 +1,38 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from typing import Annotated
+
+from fastapi import FastAPI, UploadFile, Form, File, BackgroundTasks, HTTPException
+from pydantic import EmailStr
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
+
+from app.database.database import create_db_and_tables, SessionDep
+from app.services.registration import register_patient
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@asynccontextmanager
+async def lifespan():
+    create_db_and_tables()
+    yield
+    pass
+
+
+@app.post("/register/", status_code=201)
+async def register(
+    session: SessionDep,
+    background_tasks: BackgroundTasks,
+    name: Annotated[str, Form(max_length=50)],
+    email: Annotated[EmailStr, Form()],
+    phone: Annotated[str, Form(pattern=r"^\+?[1-9]\d{1,14}$")],
+    document: UploadFile = File(...),
+) -> dict:
+    try:
+        await register_patient(session, background_tasks, name, email, phone, document)
+        return {"message": "Patient registered successfully."}
+    except Exception:
+        raise HTTPException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred during registration. Please try again later.",
+        )
